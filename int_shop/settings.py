3c3383dcf3a059df4d8c9a67dcb45983b1aadcd9
artem <tema.reida87@gmail.com>
2023-08-17 19:34:22 +0300

    add new folder...

--- int_shop/settings.py
+++ int_shop/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market',
+    'cart',
+    'shop',
+    'orders',
+    'users',
+    'products',
 ]
 
 MIDDLEWARE = [
