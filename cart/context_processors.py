85de0e00cfb6c9b7bccc04f576007c0bb2227748
artem <tema.reida87@gmail.com>
2023-09-24 22:44:08 +0300

    refresh project 24/09/23 : 22.45

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,6 +1,9 @@
-from .views import Cart
+from .views import Cart, ProductCartUser
 # импортируем класс из views
 
 def cart(request):
-    return {'cart': Cart(request)}
+    if request.user.id:
+        return {'cart': ProductCartUser(request)}
 # вернули словарь - объект корзины
+    return {'cart': Cart(request)}
+
