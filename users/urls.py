7b7a2a611d17fee7f6a144e1df2874ce770ca2d5
artem <tema.reida87@gmail.com>
2023-09-24 22:10:59 +0300

    refresh project 24/09/23 : 22.10

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-<<<<<<< HEAD
 # from .views import register.....
 from .views import *
-=======
-from .views import register, log_out, log_in
->>>>>>> 68da62b (refresh project 22/09/23)
+# from .views import register, log_out, log_in
+
 
 app_name = 'users'
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', log_in, name='login'),
     path('logout/', log_out, name='logout'),
-    #  path('logout/', log_out, name='logout'),
-    
+        
 ]
