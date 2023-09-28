edda206e32ca6cf09b13c3a608c6d8a66bd3c5d8
artem <tema.reida87@gmail.com>
2023-09-28 20:23:44 +0300

    refresh project 28/09/23

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import create_order
+from .views import create_order OrdersListView, 
 # представление create_order из views
 
 # заказы
 app_name = 'orders'
 urlpatterns = [
+    path('', OrdersListView.as_view(), name='order-list'),
     path('new_order/', create_order, name='order_create'),
+    # path('order-info/<str:pk>/', OrderDetailView.as_view(), name='order-detail'),
     
 ]
