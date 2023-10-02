727c1be97bc9c0b5677ee97c61942bd0fc8e0ae9
artem <tema.reida87@gmail.com>
2023-10-02 22:39:30 +0300

    refresh project 02.10.23 22.40

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# from .models import *
 
-# Register your models here.
+# Register your models here. регистрируем ту модель какую хотим видеть в админ панели
+
+# admin.site.register(market)
+# admin.site.register(orders)
+# admin.site.register(products)
+# admin.site.register(shop)
+# admin.site.register(shop_market)
+# admin.site.register(users)
